069ae230c519012c747655ba3cc99accced63177
Rambod Azimi <59399542+rambodazimi@users.noreply.github.com>
2024-03-16 01:04:02 -0400

    user pass added

--- src/G01JDBC.java
+++ src/G01JDBC.java
@@ -28,8 +28,8 @@ class G01JDBC {
         String url = "jdbc:db2://winter2024-comp421.cs.mcgill.ca:50000/comp421";
 
         //REMEMBER to remove your user id and password before submitting your code!!
-        String your_userid = null;
-        String your_password = null;
+        String your_userid = "cs421g01";
+        String your_password = "comp421g01";
         
         //AS AN ALTERNATIVE, you can just set your password in the shell environment in the Unix (as shown below) and read it from there.
         //$  export SOCSPASSWD=yoursocspasswd 
