93f49b97c404754a7d1c9cd6e2246bd085a3be91
Rambod Azimi <59399542+rambodazimi@users.noreply.github.com>
2024-03-18 00:53:10 -0400

    all functionalities added

--- src/G01JDBC.java
+++ src/G01JDBC.java
@@ -62,8 +62,9 @@ class G01JDBC {
             	option = scanner.nextInt();
             } catch(Exception e) {
         		System.out.println("Invalid option! Please enter a number from 1 to 6.");
+                scanner.nextLine();
+                continue;
             }
-        	scanner.nextLine();
 
             switch(option) {
             	case 1: // Add a new event
@@ -83,7 +84,7 @@ class G01JDBC {
                         startDate = new Date(utilDate.getTime());
                     } catch (ParseException e) {
                         System.out.println("Invalid date format. Please enter the date in YYYY-MM-DD format.");
-                        return;
+                        continue;
                     }
                     
                     System.out.print("Start time (HH:mm:ss): ");
@@ -98,13 +99,13 @@ class G01JDBC {
                         startTime = new Time(utilDate.getTime());
                     } catch (ParseException e) {
                         System.out.println("Invalid time format. Please enter the time in HH:mm:ss format.");
-                        return;
+                        continue;
                     }
                     
-                    System.out.print("Duration(integer number): ");
+                    System.out.print("Duration (minutes): ");
                     int duration = scanner.nextInt();
                     
-                    System.out.print("Type ID (integer): ");
+                    System.out.print("Type ID (layout): ");
                     int typeId = scanner.nextInt();
 
                     
@@ -137,7 +138,7 @@ class G01JDBC {
                         dob = new Date(utilDate.getTime());
                     } catch (ParseException e) {
                         System.out.println("Invalid date format. Please enter the date in YYYY-MM-DD format.");
-                        return;
+                        continue;
                     }
             		
                     System.out.print("Username: ");
@@ -161,7 +162,7 @@ class G01JDBC {
                         expiryDate = new Date(utilDate.getTime());
                     } catch (ParseException e) {
                         System.out.println("Invalid date format. Please enter the date in YYYY-MM-DD format.");
-                        return;
+                        continue;
                     }
                     
              		System.out.print("Location: ");
@@ -189,9 +190,10 @@ class G01JDBC {
             	case 3: // Purchase a ticket
             		
             		System.out.print("Enter your email address: "); // we assume the user already exists in the database
+            		scanner.nextLine();
             		String email = scanner.nextLine();
             		
-            		// find UserID from email address
+            		// find UserID from USERS relation
             		String userQuery = "SELECT UserID FROM USERS WHERE EmailAddress='" + email + "'";
                     java.sql.ResultSet result = statement.executeQuery(userQuery);
                     
@@ -200,8 +202,13 @@ class G01JDBC {
                       userid = result.getInt(1);
                     }
             		
+                    if(userid == -1) {
+                    	System.out.println("User not found in the database. Please register first!");
+                    	continue;
+                    }
+                    
             		// Event Type
-            		System.out.print("Select: 1. Performances \t 2. Sport Games");
+            		System.out.print("Select: 1. Performances    2. Sport Games: ");
             		int eventType = scanner.nextInt();
             		
             		if(eventType != 1 && eventType != 2) {
@@ -209,6 +216,7 @@ class G01JDBC {
             			break;
             		}
             		
+            		// Show the list of available events
             		if(eventType == 1) {
             			printPerformances(statement);
             		}
@@ -220,22 +228,92 @@ class G01JDBC {
             		System.out.println("\nSelect EID in which you wish to purchase a ticket: ");
             		int eidPurchase = scanner.nextInt(); // we assume the EID is already correct
             		
+            		if(eventType == 1) {
+            			String eidQueryp = "SELECT EID FROM PERFORMANCES WHERE EID='" + eidPurchase + "'";
+            			java.sql.ResultSet result2 = statement.executeQuery(eidQueryp);
+            			eidPurchase = -1;
+                        while (result2.next()){
+                        	eidPurchase = result2.getInt(1);
+                        }
+                		
+                        if(eidPurchase == -1) {
+                        	System.out.println("Performance not found in the database. Please try again!");
+                        	continue;
+                        }
+            		}
+            		
+            		if(eventType == 2) {
+            			String eidQueryp = "SELECT EID FROM SPORTGAMES WHERE EID='" + eidPurchase + "'";
+            			java.sql.ResultSet result2 = statement.executeQuery(eidQueryp);
+            			eidPurchase = -1;
+                        while (result2.next()){
+                        	eidPurchase = result2.getInt(1);
+                        }
+                		
+                        if(eidPurchase == -1) {
+                        	System.out.println("Sport Game not found in the database. Please try again!");
+                        	continue;
+                        }
+            		}
+                    
             		// List of available seats
             		printSeats(statement);
             		
             		System.out.println("\nSelect your section number: ");
             		int secNumber = scanner.nextInt();
+            		
+        			String sectionQuery = "SELECT SECTIONNUMBER FROM SEATS WHERE SECTIONNUMBER='" + secNumber + "'";
+        			java.sql.ResultSet resultSectionQuery = statement.executeQuery(sectionQuery);
+        			secNumber = -1;
+                    while (resultSectionQuery.next()){
+                    	secNumber = resultSectionQuery.getInt(1);
+                    }
+            		
+                    if(secNumber == -1) {
+                    	System.out.println("Section Number not found in the database. Please try again!");
+                    	continue;
+                    }
 
             		System.out.println("\nSelect your seat number: ");
             		int seatNum = scanner.nextInt();
             		
+        			String seatQuery = "SELECT SEATNUMBER FROM SEATS WHERE SEATNUMBER='" + seatNum + "'";
+        			java.sql.ResultSet resultSeatQuery = statement.executeQuery(seatQuery);
+        			seatNum = -1;
+                    while (resultSeatQuery.next()){
+                    	seatNum = resultSeatQuery.getInt(1);
+                    }
+            		
+                    if(seatNum == -1) {
+                    	System.out.println("Seat Number not found in the database. Please try again!");
+                    	continue;
+                    }
+            		
             		System.out.println("\nSelect your row number: ");
             		int rowNum = scanner.nextInt();
             		
+        			String rowQuery = "SELECT ROWNUMBER FROM SEATS WHERE ROWNUMBER='" + rowNum + "'";
+        			java.sql.ResultSet resultRowQuery = statement.executeQuery(rowQuery);
+        			rowNum = -1;
+                    while (resultRowQuery.next()){
+                    	rowNum = resultRowQuery.getInt(1);
+                    }
+            		
+                    if(rowNum == -1) {
+                    	System.out.println("Row Number not found in the database. Please try again!");
+                    	continue;
+                    }
+            		
+                    // Calculating price ticket
             		double highestPrice = 500;
             		
             		double price = highestPrice / rowNum; // As the row increases, the price drops
             		
+            		System.out.println("-------------------------------------");
+            		System.out.println("Event ID: " + eidPurchase);
+            		System.out.println("Section Number: " + secNumber);
+            		System.out.println("Seat Number: " + seatNum);
+            		System.out.println("Row Number: " + rowNum);
             		System.out.println("Total Price: $" + price);
             		
                     System.out.print("Confirm purchase (yes/no): ");
@@ -259,6 +337,9 @@ class G01JDBC {
                                                 
                         addTicket(statement, ticketID, price, 1, userid, sqlDate, eidPurchase);
                         
+                        System.out.println("Purchase successful!");
+                        System.out.println("Ticket ID: " + ticketID);
+ 
                         // Fill the seat so that no one else can purchase that same seat
                         String removeQuery = "DELETE FROM SEATS WHERE SectionNumber='" + secNumber + "'" + " AND SeatNumber='" + seatNum + "'" + " AND RowNumber='" + rowNum + "'";
                         statement.executeUpdate(removeQuery);  
@@ -271,6 +352,7 @@ class G01JDBC {
             		
             	case 4: // Add a new sponsor
             		System.out.print("Sponsor Name: ");
+            		scanner.nextLine();
              		String sponsorName = scanner.nextLine();
              		
              		System.out.print("Sponsor Type: ");
@@ -290,11 +372,76 @@ class G01JDBC {
                     
                     int eidRelation = scanner.nextInt();
                     
+                    String eidQuery = "SELECT EID FROM EVENTS WHERE EID='" + eidRelation + "'";
+        			java.sql.ResultSet resultEidQuery = statement.executeQuery(eidQuery);
+        			eidRelation = -1;
+                    while (resultEidQuery.next()){
+                    	eidRelation = resultEidQuery.getInt(1);
+                    }
+            		
+                    if(eidRelation == -1) {
+                    	System.out.println("EID not found in the database. Please try again!");
+                    	continue;
+                    }
+
             		addSponsor(statement, sid, sponsorName, sponsorType, eidRelation);
             		break;
             		
             	case 5: // Analytics
             		// Sub Menu
+                	System.out.println("\nAnalytics");
+                    System.out.println("1. View all events");
+                    System.out.println("2. View performances");
+                    System.out.println("3. View sport games");
+                    System.out.println("4. View all users");
+                    System.out.println("5. View registered users");
+                    System.out.println("6. View all purchased tickets");
+                    System.out.println("7. View purchased tickets of an event");
+                    System.out.println("8. View all sponsors");
+                    System.out.println("9. Return to main menu");
+                    System.out.print("Enter your choice: ");
+                    int option2 = 0;
+                    try {
+                    	option2 = scanner.nextInt();
+                    } catch(Exception e) {
+                		System.out.println("Invalid option! Try again!");
+                        scanner.nextLine();
+                        continue;
+                    }
+                    if(option2 == 1) {
+                    	printEvents(statement);
+                    }
+                    else if(option2 == 2) {
+                    	printPerformances(statement);
+                    }
+                    else if(option2 == 3) {
+                    	printSportGames(statement);
+                    }
+                    else if(option2 == 4) {
+                    	printUsers(statement);
+                    }
+                    else if(option2 == 5) {
+                    	printRegisteredUsers(statement);
+                    }
+                    else if(option2 == 6) {
+                    	printTickets(statement);
+                    }
+                    else if(option2 == 7) {
+                    	System.out.println("Select EID: ");
+                    	int inputEID = scanner.nextInt();
+                    	printTicketsOfEvent(statement, inputEID);
+                    }
+                    else if(option2 == 8) {
+                    	printSponsors(statement);
+                    }
+                    else if(option2 == 9) {
+                    	continue;
+                    }
+                    else {
+                		System.out.println("Invalid option! Try again!");
+                		continue;
+                    }
+                    
             		break;
             		
             	case 6:
@@ -342,7 +489,6 @@ class G01JDBC {
           }   
     	
     	// Adding registered user to the database
-    	
     	try{
     	  String insertSQL = "INSERT INTO REGISTEREDUSERS (UserID, Username, Password, CreditCard, ExpiryDate, Location, PreferredType, PreferredGenre) " +
                     "VALUES (" + userID + ",'" + username + "','" + password + "','" + creditCard + "','" + expiryDate + "','" + location + "','" + preferredType + "','" + preferredGenre + "')";
@@ -483,6 +629,31 @@ class G01JDBC {
           }
     }
     
+    public static void printUsers(Statement statement) {
+    	try {
+            String querySQL = "SELECT * FROM Users";
+            java.sql.ResultSet rs = statement.executeQuery(querySQL);
+            
+            System.out.println("--------------------------------------------");
+            System.out.println("| UserID |    EmailAddress     |    DOB    |");
+            System.out.println("--------------------------------------------");
+            
+            while (rs.next()){
+              int userID = rs.getInt(1);
+              String emailAddress = rs.getString(2);
+              String dob = rs.getString(3);
+              System.out.printf("| %-7d| %-20s| %-10s|%n", userID, emailAddress, dob);
+            }
+            System.out.println("--------------------------------------------");
+          }catch(SQLException e) {
+            int sqlCode = e.getErrorCode(); // Get SQLCODE
+            String sqlState = e.getSQLState(); // Get SQLSTATE
+                  
+            System.out.println("Code: " + sqlCode + "  sqlState: " + sqlState);
+            System.out.println(e);
+          }
+    }
+    
     public static void printRegisteredUsers(Statement statement) {
     	try {
             String querySQL = "SELECT * FROM RegisteredUsers";
@@ -567,6 +738,36 @@ class G01JDBC {
             System.out.println(e);
           }
     }
+    
+    public static void printTicketsOfEvent(Statement statement, int inputEID) {
+    	try {
+            String eidQuery = "SELECT * FROM TICKETS WHERE EID='" + inputEID + "'";
+            java.sql.ResultSet rs = statement.executeQuery(eidQuery);
+            
+            System.out.println("-----------------------------------------------------------------------");
+            System.out.println("| TicketID | Price | DesignatedEntrance | UserID | PurchaseDate | EID |");
+            System.out.println("-----------------------------------------------------------------------");
+            
+            while (rs.next()){
+              int ticketId = rs.getInt(1);
+              float price = rs.getFloat(2);
+              int designatedEntrance = rs.getInt(3);
+              int userId = rs.getInt(4);
+              Date purchaseDate = rs.getDate(5);
+              int eid = rs.getInt(6);
+
+              System.out.printf("| %-9d| %-6.2f| %-19d| %-7d| %-13s| %-4d|%n", ticketId, price, designatedEntrance, userId, purchaseDate, eid);
+            }
+            System.out.println("-----------------------------------------------------------------------");
+          }catch(SQLException e) {
+            int sqlCode = e.getErrorCode(); // Get SQLCODE
+            String sqlState = e.getSQLState(); // Get SQLSTATE
+                  
+            System.out.println("Code: " + sqlCode + "  sqlState: " + sqlState);
+            System.out.println(e);
+          }
+    }
+
     
     public static void printSeats(Statement statement) {
     	try {
